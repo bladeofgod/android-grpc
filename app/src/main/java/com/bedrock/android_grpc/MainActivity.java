db87b9aae6a3df63d25255a4d961b6066a70a3b6
bladeofgod <450103187@qq.com>
2020-05-03 15:41:10 +0800

    add grpc plugin

--- app/src/main/java/com/bedrock/android_grpc/MainActivity.java
+++ app/src/main/java/com/bedrock/android_grpc/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import io.grpc.ManagedChannel;
+import io.grpc.examples.helloworld.GreeterGrpc;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
